a0383762a96efe5747b56bf8dd307fb25fe0d443
Jasmine Justin <jasminejustin7@gmail.com>
2024-10-24 08:38:53 -0400

    getting started on apptDate Date Picker

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+
 
 public class HelloController {
     @FXML
@@ -11,4 +16,13 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private Button bt_loadProviders;
+
+
+    @FXML
+    private DatePicker db_apptDateSC;
+
+    
 }
