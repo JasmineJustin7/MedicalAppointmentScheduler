1707c5d95de0ea3f03d2a7e82c8ccc6e25a65eeb
Jasmine Justin <jasminejustin7@gmail.com>
2024-10-24 12:27:03 -0400

    Radio Button is now single select for Schedule/Cancel tab

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
+import classes.Provider;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
 
 
 public class HelloController {
@@ -22,7 +29,34 @@ public class HelloController {
 
 
     @FXML
+    private ComboBox<Provider> cb_providerSC;
+
+    @FXML
     private DatePicker db_apptDateSC;
 
-    
+    @FXML
+    void appointmentDatePicker(ActionEvent event){
+        DatePicker datePicker = new DatePicker();
+        LocalDate today = LocalDate.now();
+        LocalDate sixMonthsLater = today.plusMonths(6);
+
+    }
+
+    //@Override
+    public void initialize(){
+        //ObservableList<Provider> providerObservableList = FXCollections.observableArrayList(Provider.get);
+       //cb_providerSC.setItems(providerObservableList);
+
+    }
+
+    /**will load up list of providers and print them in provider combo box*/
+    //@FXML
+    /*void addProvidersToComboBox(ActionEvent event){
+        cb_providerSC.getItems();
+
+    }*/
+
+
+
+
 }
